93bca32549e758b46ad0d8f3624664f169bf63f6
Ryan Henderson <wrhenders@gmail.com>
2022-10-31 15:22:39 -0500

    updated to change incorrect input response

--- app/src/main/java/com/kitchen/recipeconverter/ui/gramit/GramItAdapter.kt
+++ app/src/main/java/com/kitchen/recipeconverter/ui/gramit/GramItAdapter.kt
@@ -1,19 +1,25 @@
 package com.kitchen.recipeconverter.ui.gramit
 
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.widget.addTextChangedListener
 import androidx.core.widget.doAfterTextChanged
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import com.kitchen.recipeconverter.R
+import com.kitchen.recipeconverter.data.Converter
 import com.kitchen.recipeconverter.data.GramItItem
 import com.kitchen.recipeconverter.data.Ingredient
+import com.kitchen.recipeconverter.data.IngredientList
 
 class GramItAdapter(private val itemsList: List<GramItItem>,
                     private val unitAdapter: ArrayAdapter<String>,
@@ -25,6 +31,7 @@ class GramItAdapter(private val itemsList: List<GramItItem>,
         val quantityText: TextInputEditText = view.findViewById(R.id.quantity_text)
         val unitText: AutoCompleteTextView = view.findViewById(R.id.unit_text)
         val ingredientText: AutoCompleteTextView = view.findViewById(R.id.ingredient_text)
+        val ingredientLayout: TextInputLayout = view.findViewById(R.id.ingredient_menu)
         var currentQuantity = quantityText.toString()
         var currentUnit = unitText.toString()
         var currentIngredient = ingredientText.toString()
@@ -40,31 +47,81 @@ class GramItAdapter(private val itemsList: List<GramItItem>,
         // Add the adapters for dropdown and autocomplete menus
         holder.unitText.setAdapter(unitAdapter)
         holder.ingredientText.setAdapter(ingredientAdapter)
+        holder.ingredientText.validator = IngredientValidator()
 
         // Set the fields to correct current state
         holder.quantityText.setText(itemsList[position].quantity.toString())
         holder.unitText.setText(itemsList[position].unit)
         holder.ingredientText.setText(itemsList[position].ingredient)
 
+        fun onClick(){
+            val currentItem = GramItItem(holder.currentQuantity,holder.currentUnit, holder.currentIngredient)
+            onItemClicked(currentItem, position)
+        }
+
+        fun changeStrokeColor(type: String): ColorStateList{
+            val errorColor = Color.parseColor("#B00020")
+            val defaultColor = Color.parseColor("#60000000")
+
+            val states = arrayOf(
+                intArrayOf(android.R.attr.state_focused),
+                intArrayOf(android.R.attr.state_hovered),
+                intArrayOf(android.R.attr.state_enabled),
+                intArrayOf()
+            )
+            val colors =
+                if (type == "error") {
+                 intArrayOf(defaultColor, defaultColor, defaultColor, errorColor)
+                } else {
+                    intArrayOf(defaultColor, defaultColor, defaultColor, defaultColor)
+                }
+            return ColorStateList(states, colors)
+        }
+
         holder.quantityText.doAfterTextChanged {
-            holder.currentQuantity = it.toString()
-            onClick(holder.currentQuantity, holder.currentUnit, holder.currentIngredient, position)
+            val updatedQuantity = it.toString()
+            if (updatedQuantity.toDoubleOrNull() is Double) {
+                holder.currentQuantity = updatedQuantity
+                onClick()
+            } else {
+                holder.quantityText.error = "Enter Number"
+            }
         }
         holder.unitText.doAfterTextChanged {
-            holder.currentUnit = it.toString()
-            onClick(holder.currentQuantity, holder.currentUnit, holder.currentIngredient, position)
+            val updatedUnit = it.toString()
+            if (Converter().getUnitType(updatedUnit).isNotEmpty()){
+                holder.currentUnit = updatedUnit
+                onClick()
+            } else {
+                holder.unitText.error = "Not Valid"
+            }
         }
         holder.ingredientText.doAfterTextChanged {
-            holder.currentIngredient = it.toString()
-            onClick(holder.currentQuantity, holder.currentUnit, holder.currentIngredient, position)
+            val updatedIngredient = it.toString()
+            if (holder.ingredientText.validator.isValid(updatedIngredient)) {
+                holder.ingredientLayout.setBoxStrokeColorStateList(changeStrokeColor("default"))
+                holder.currentIngredient = updatedIngredient
+                onClick()
+            } else {
+                holder.ingredientLayout.setBoxStrokeColorStateList(changeStrokeColor("error"))
+            }
         }
-
-    }
-
-    private fun onClick(quantity: String, unit: String, ingredient: String, position: Int){
-        val currentItem = GramItItem(quantity, unit, ingredient)
-        onItemClicked(currentItem, position)
     }
 
     override fun getItemCount() = itemsList.size
 }
+
+class IngredientValidator : AutoCompleteTextView.Validator {
+    override fun isValid(text: CharSequence?): Boolean {
+        if(text.toString() in IngredientList().getList().map{ing->ing.name}){
+            return true
+        }
+        return false
+    }
+
+    override fun fixText(invalidText: CharSequence?): CharSequence {
+        return invalidText ?: ""
+    }
+
+}
+
