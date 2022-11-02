a4b43854f953ec5a5d21b52f27ce6cf10ed97d49
Ryan Henderson <wrhenders@gmail.com>
2022-11-02 10:38:33 -0500

    added methods for interacting with database

--- app/src/main/java/com/kitchen/recipeconverter/ui/recipelist/RecipeListViewModel.kt
+++ app/src/main/java/com/kitchen/recipeconverter/ui/recipelist/RecipeListViewModel.kt
@@ -1,15 +1,55 @@
 package com.kitchen.recipeconverter.ui.recipelist
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.asLiveData
+import androidx.lifecycle.*
 import com.kitchen.recipeconverter.data.recipe.Recipe
 import com.kitchen.recipeconverter.data.recipe.RecipeDao
+import kotlinx.coroutines.launch
 
 class RecipeListViewModel(private val recipeDao: RecipeDao) : ViewModel() {
 
     val allRecipes: LiveData<List<Recipe>> = recipeDao.getRecipes().asLiveData()
+
+    private fun insertRecipe(recipe: Recipe){
+        viewModelScope.launch {
+            recipeDao.insert(recipe)
+        }
+    }
+
+    private fun deleteRecipe(recipe: Recipe){
+        viewModelScope.launch {
+            recipeDao.delete(recipe)
+        }
+    }
+
+    private fun updateRecipe(recipe: Recipe){
+        viewModelScope.launch {
+            recipeDao.update(recipe)
+        }
+    }
+
+    fun addNewRecipe(recipeTitle: String, recipeIngredients: String, recipeMethod: String) {
+        val currentDate = System.currentTimeMillis().toInt()
+        val newRecipe = Recipe(recipeTitle=recipeTitle, recipeIngredients = recipeIngredients, recipeMethod = recipeMethod, dateModified = currentDate)
+        insertRecipe(newRecipe)
+    }
+
+    fun updateRecipe(id: Int, recipeTitle: String, recipeIngredients: String, recipeMethod: String) {
+        val currentDate = System.currentTimeMillis().toInt()
+        val updatedRecipe = Recipe(id, recipeTitle, recipeIngredients, recipeMethod, dateModified = currentDate)
+        updateRecipe(updatedRecipe)
+    }
+
+    fun getRecipe(id: Int): LiveData<Recipe> {
+        return recipeDao.getRecipe(id).asLiveData()
+    }
+
+    fun isEntryValid(recipeTitle: String, recipeIngredients: String, recipeMethod: String) : Boolean {
+        if (recipeTitle.isBlank() || recipeIngredients.isBlank() || recipeMethod.isBlank()){
+            return false
+        }
+        return true
+    }
+
 }
 
 class RecipeListViewModelFactory(private val recipeDao: RecipeDao) : ViewModelProvider.Factory {
